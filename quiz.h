044bdac4a096cccce3bc3bb9d90d813048e1188f
Kulbir <kulbir4721@gmail.com>
2025-10-04 19:45:00 +0545

    commiting quiz.h test

--- quiz.h
+++ quiz.h
@@ -1,40 +1,44 @@
 #ifndef QUIZ_H
 #define QUIZ_H
-
-# include <iostream>
-# include <vector>
-# include <chrono>
-# include <thread>
+#include <iostream>
 #include <sqlite3.h>
+#include <vector>
+#include <chrono>
+#include <thread>
 using namespace std;
 
-class Question {
-public:
-    string questionText;
-    vector<string> options;
-    char correctOption;
+typedef enum GameMode{
+CLASSIC=1,
+SURVIVAL,
+TIMED
+}GameMode;
 
-    Question() {}
-    Question(string q, vector<string> opts, char ans) {
-        questionText = q;
-        options = opts;
-        correctOption = ans;
-    }
+class Question{
+public:
+string questionText;
+vector<string> options;
+char correctOption;
+string difficulty;
+string category;
+
+Question(){};
+ Question(string q, vector<string> opts, char ans, string diff, string cat)
+        : questionText(q), options(opts), correctOption(ans), difficulty(diff), category(cat) {}
 };
-class Quiz {
-private:
-    vector<Question> questions;
-    int score;
-    Mode gameMode;
-    int numQuestions;
 
+class Quiz{
 public:
-    Quiz();
-
-    void setMode(Mode m);
-    void classic();
-    void loadQuestions(sqlite3 *db, int limit);
-    void startQuiz();
-    void displayResult(chrono::duration<double> totalTime);
+vector<Question> questions;
+int score;
+GameMode gamemode;
+Quiz();
+  void reset();
+  void displayscore();
+  void setmode(GameMode gm);
+  void loadquestions(sqlite3 *db, int limit, const string &difficulty="", const string &category="");
+  void savescore(sqlite3 *db, const string &playername);
+  void classic(sqlite3 *db, const string &difficulty,const string &category);
 };
 #endif
+
+
